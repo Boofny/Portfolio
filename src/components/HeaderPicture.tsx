4e0b4c39e3aa7ca966af7d1e831d6e74d560dba6
Boofny <Brandonpizano7557@gmail.com>
2026-02-04 18:07:09 -0500

    added some animation for github and linkedin buttons

--- src/components/HeaderPicture.tsx
+++ src/components/HeaderPicture.tsx
@@ -7,15 +7,15 @@ function HeaderPicture(){
       <div className="w-full h-full flex justify-center items-center gap-5">
         <div className="w-10 h-10 flex justify-center items-center">
           <a href="https://github.com/Boofny" target="_blank" rel="noopener noreferrer">
-            <button>
-              <img className="bg-white rounded-2xl border-2 border-OneBlue" src="https://cdn-icons-png.flaticon.com/128/5968/5968866.png" alt="github" />
+            <button className="cursor-pointer">
+              <img className="hover:scale-110 hover:-translate-y-1.5 bg-white rounded-2xl ease-in-out transition hover:border-OneGreen border-2 border-OneBlue" src="https://cdn-icons-png.flaticon.com/128/5968/5968866.png" alt="github" />
             </button>
           </a>
         </div>
         <div className="w-10 h-10 flex justify-center items-center">
           <a href="https://google.com" target="_blank" rel="noopener noreferrer">
-            <button>
-              <img className="bg-white border-2 border-OneBlue" src="https://cdn-icons-png.flaticon.com/128/121/121509.png" alt="linkedin" />
+            <button className="cursor-pointer">
+              <img className="bg-white border-2 border-OneBlue hover:scale-110 hover:-translate-y-1.5 ease-in-out transition hover:border-OneGreen" src="https://cdn-icons-png.flaticon.com/128/121/121509.png" alt="linkedin" />
             </button>
           </a>
         </div>
