ed66d4d77cf605127d09078658ee1eb6d64575e5
Boofny <Brandonpizano7557@gmail.com>
2026-02-06 21:59:49 -0500

    some more project layout and rethinking of the layout

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,21 +1,38 @@
+import { useState } from "react"
+
+function GoLive() { // comps
+  return <h2>Home content</h2>
+}
+
+function AsciiIt() {
+  return <h2>Profile content</h2>
+}
+
+function UrlShorter() {
+  return <h2>Settings content</h2>
+}
+
+type Tab = "golive" | "asciiIt" | "urlshorter"
+
 function Projects(){
+  const [activeTab, setActiveTab] = useState<Tab>("golive")
   return(
-    <div>
-      <div className="w-full h-full flex justify-center items-top pl-25 pr-25 pt-1 gap-20 ">
-        <div className="w-full h-125 border border-white">
-          <p className="text-white">project</p>
+    <div className="w-full h-full flex justify-center items-top pl-25 pr-25 pt-1 gap-20 ">
+      <div className="w-full h-125 border border-white flex justify-center items-center flex-col bg-OneDarkGray">
+        <div className="w-full h-15 flex justify-center items-center text-white">
+        {/* Should be on click the color will change for the tab making it seem like the tab is connected to the content and ofc add hover effects and stuff*/}
+          <button className="border border-white w-full h-full" onClick={() => setActiveTab("golive")}>Go Live</button> 
+          <button className="border border-white w-full h-full" onClick={() => setActiveTab("asciiIt")}>Ascii It</button> 
+          <button className="border border-white w-full h-full" onClick={() => setActiveTab("urlshorter")}>URL Shortner</button> 
         </div>
-        <div className="w-100 h-125 border border-white">
-          <p className="text-white">Tech stack</p>
+        <div className="w-full h-full text-white flex justify-center items-center">
+           {activeTab === "golive" && <GoLive/>}
+           {activeTab === "asciiIt" && <AsciiIt/>}
+           {activeTab === "urlshorter" && <UrlShorter/>}
         </div>
       </div>
-      <div className="w-full h-full flex justify-center items-top pl-25 pr-25 pt-20 gap-20 ">
-        <div className="w-100 h-125 border border-white">
-          <p className="text-white">Tech stack</p>
-        </div>
-        <div className="w-full h-125 border border-white">
-          <p className="text-white">project</p>
-        </div>
+      <div className="w-100 h-125 border border-white bg-OneDarkGray">
+        <p className="text-white">Tech stack</p>
       </div>
     </div>
   )
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -4,53 +4,6 @@ import About from "../components/About.tsx"
 import Projects from "../components/Projects.tsx"
 import Experience from "../components/Experience.tsx"
 
-// COLOR LIST 
-// RED          #e06c75	
-// YELLOW       #e5c07b	
-// GREEN        #98c379	
-// CYAN         #56b6c2	
-// BLUE         #61afef
-// GREY         #181818
-
-
-// function Home() { // comps
-//   return <h2>Home content</h2>
-// }
-//
-// function Profile() {
-//   return <h2>Profile content</h2>
-// }
-//
-// function Settings() {
-//   return <h2>Settings content</h2>
-// }
-//
-// type Tab = "home" | "profile" | "settings"
-//
-// function Normal() {
-//   const [activeTab, setActiveTab] = useState<Tab>("home")
-//
-//   return (
-//     <div className="flex justify-center items-center flex-col">
-//     <Header></Header>
-//       <div className="flex justify-center items-center h-screen flex-col">
-//         <div className="flex justify-center items-center gap-2 text-white">
-//           <button onClick={() => setActiveTab("home")}>Home</button>
-//           <button onClick={() => setActiveTab("profile")}>Profile</button>
-//           <button onClick={() => setActiveTab("settings")}>Settings</button>
-//         </div>
-//         <div className="flex justify-center items-center gap-2 text-red-500">
-//           {activeTab === "home" && <Home />}
-//           {activeTab === "profile" && <Profile />}
-//           {activeTab === "settings" && <Settings />}
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-//
-// export default Normal;
-
 function Homepage(){
   return(
     <div className="m-0">
