99945a9da909273959b5b7faedccd20283dbd9b6
Boofny <Brandonpizano7557@gmail.com>
2026-05-04 20:56:24 -0400

    making some of the skills grid, note1: make the grid squares all time note2 fix project width

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,12 +5,24 @@ function Skills(){
         <legend className="ml-4 md:ml-10 text-OneYellow font-hack text-lg font-bold tracking-widest">
           [3]Skills
         </legend>
-          <div className="">
-            <p className="text-white">Skills hllo wodwdopihwoih</p>
+          <div className="grid grid-cols-3 gap-3 w-full h-full text-white p-2 pt-0">
+            <div className="border w-full h-full ">1</div>
+            <div className="border w-full h-full ">1</div>
+            <div className="border w-full h-full ">1</div>
+            <div className="border w-full h-full ">1</div>
+            <div className="border w-full h-full ">1</div>
+            <div className="border w-full h-full ">1</div>
+            <div className="border w-full h-full ">1</div>
+            <div className="border w-full h-full ">1</div>
+            <div className="border w-full h-full ">1</div>
+            <div className="border w-full h-full ">1</div>
+            <div className="border w-full h-full ">1</div>
+            <div className="border w-full h-full ">1</div>
           </div>
       </fieldset>
     </div>
   )
 }
-
+// potential skills
+// go, java, linux, nextjs, react, postgres, mysql, supabase, astrojs, tailwindcss, bash?, python, git?, TS?, graphql? <- strech
 export default Skills;
